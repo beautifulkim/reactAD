c1dcb4ad70ac84abef7fef4118ed1ffcbd369078
kmk <ga2d2000@nate.com>
2023-01-16 20:35:20 +0900

    Scrollspy

--- README.md
+++ README.md
@@ -68,3 +68,10 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
+----------------------
+
+포트설정 URL
+https://7942yongdae.tistory.com/4
--- src/component/Navi.js
+++ src/component/Navi.js
@@ -2,19 +2,25 @@ import React from 'react';
 import logo from '../logo.svg';
 import navicss from '../css/navi.module.css';
 import { Link } from 'react-router-dom';
+import Scrollspy from 'react-scrollspy';
 
 // npm install react-router-dom
 // npm install react-bootstrap bootstrap
 //npm i react-scrollspy
-
 function Navi() {
     return (
-        <div className={ `${navicss.hd} fixed-top borde-bottom py-3 bg-white` }>
+        <div className={ `${navicss.hd} fixed-top borde-bottom py-3 px-4 bg-white` }>
             <h1>
                 <Link to="/">
                     <img src={ logo } />
                 </Link>
             </h1>
+            <Scrollspy>
+                <li><Link to='/about'>뇌세김소개</Link></li>
+                <li><a href='#procss'>뇌새김의 학습원리</a></li>
+                <li><a href='#review'>뇌새김의 후기</a></li>
+                <li><a href='#qna'>문의하기</a></li>
+            </Scrollspy>
         </div>
     );
 }
