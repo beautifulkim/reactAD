c66a787beaa285acc6501d2e56c4c571a33b1ec9
kmk <ga2d2000@nate.com>
2023-01-16 19:54:56 +0900

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navi from './component/Navi';
+
+function App() {
+  return (
+    <div className="App">
+      <Navi></Navi>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Navi.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import navicss from '../css/navi.module.css';
+
+
+function Navi() {
+    return (
+        <div className={ navicss.hd }>Navi</div>
+    );
+}
+
+export default Navi;
