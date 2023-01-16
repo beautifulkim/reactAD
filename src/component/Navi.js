5d48ffece9077942b22d4673407e508a90c0d3b7
kmk <ga2d2000@nate.com>
2023-01-16 20:23:04 +0900

    네비에 로고 뿌리기

--- src/component/Navi.js
+++ src/component/Navi.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import logo from '../logo.svg';
 import navicss from '../css/navi.module.css';
+import { Link } from 'react-router-dom';
 
+// npm install react-router-dom
+// npm install react-bootstrap bootstrap
+//npm i react-scrollspy
 
 function Navi() {
     return (
-        <div className={ navicss.hd }>Navi</div>
+        <div className={ `${navicss.hd} fixed-top borde-bottom py-3 bg-white` }>
+            <h1>
+                <Link to="/">
+                    <img src={ logo } />
+                </Link>
+            </h1>
+        </div>
     );
 }
 
