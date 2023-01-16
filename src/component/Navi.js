2a3eb80264e48720728e057e601c794f41ebe260
kmk <ga2d2000@nate.com>
2023-01-16 20:46:11 +0900

    style 추가

--- src/component/Navi.js
+++ src/component/Navi.js
@@ -9,18 +9,22 @@ import Scrollspy from 'react-scrollspy';
 //npm i react-scrollspy
 function Navi() {
     return (
-        <div className={ `${navicss.hd} fixed-top borde-bottom py-3 px-4 bg-white` }>
+        <div className={ `${navicss.hd} fixed-top border-bottom py-3 px-4 bg-white d-flex justify-content-between align-items-center` }>
             <h1>
                 <Link to="/">
                     <img src={ logo } />
                 </Link>
             </h1>
-            <Scrollspy>
+            <Scrollspy className='d-flex'>
                 <li><Link to='/about'>뇌세김소개</Link></li>
                 <li><a href='#procss'>뇌새김의 학습원리</a></li>
                 <li><a href='#review'>뇌새김의 후기</a></li>
                 <li><a href='#qna'>문의하기</a></li>
             </Scrollspy>
+            <ul id="sns" className='d-flex'>
+                <li><a href='http://www.youtube.com' target='_blank'>유튜브</a></li>
+                <li><a href='http://www.youtube.com' target='_blank'>자료실</a></li>
+            </ul>
         </div>
     );
 }
