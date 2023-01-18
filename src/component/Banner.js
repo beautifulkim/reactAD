ba02b509120255474d440c8b535ecb101561440e
kmk <ga2d2000@nate.com>
2023-01-18 19:20:20 +0900

    Swiper 1

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
 
-export default function Banner() {
+function Banner() {
     return (
-        <div style={{heght: '100vh'}} className='bg-light'>Banner</div>
+        <Swiper
+            spaceBetween={50}
+            slidesPerView={3}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            
+        </Swiper>
     )
 }
+
+export default Banner;
