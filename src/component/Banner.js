9339fcf6b84ce295089c29c7bb7f9ffdad65ad5e
kmk <ga2d2000@nate.com>
2023-01-18 19:38:40 +0900

    800px

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -5,29 +5,31 @@ import "swiper/css/pagination";
 // Import Swiper styles
 import 'swiper/css';
 
-function Banner() {
+function Banner( props ) {
     return (
-        <Swiper
-            modules={[ Pagination, Autoplay ]}
-            spaceBetween={ 50 }
-            slidesPerView={ 2 }
-            autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-            }}
-            centeredSlides={ true }
-            pagination={{
-                type: "franction", 
-                // clickable: true,
-            }}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={( swiper ) => console.log('swiper 테스트 : ', swiper)}
-        >
-            <SwiperSlide>Slide 1</SwiperSlide> 
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            
-        </Swiper>
+        <div id={ props.objid } className='h-100'>
+            <Swiper
+                modules={[ Pagination, Autoplay ]}
+                spaceBetween={ 50 }
+                slidesPerView={ 2 }
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                centeredSlides={ true }
+                pagination={{
+                    type: "franction", 
+                    // clickable: true,
+                }}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={( swiper ) => console.log('swiper 테스트 : ', swiper)}
+            >
+                <SwiperSlide className='d-flex flex-column jutify-content-center align-content-center' style={ { height : '800px' } }>Slide 1</SwiperSlide> 
+                <SwiperSlide className='d-flex flex-column jutify-content-center align-content-center' style={ { height : '800px' } }>Slide 1</SwiperSlide> 
+                <SwiperSlide className='d-flex flex-column jutify-content-center align-content-center' style={ { height : '800px' } }>Slide 1</SwiperSlide> 
+                
+            </Swiper>
+        </div>
     )
 }
 
