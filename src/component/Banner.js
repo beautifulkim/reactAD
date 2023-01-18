906d01eb47cd1f7eb80497dfee2714941380c2c3
kmk <ga2d2000@nate.com>
2023-01-18 19:32:31 +0900

    Swiper option

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Autoplay } from 'swiper';
+import "swiper/css/pagination";
 // Import Swiper styles
 import 'swiper/css';
 
 function Banner() {
     return (
         <Swiper
-            spaceBetween={50}
-            slidesPerView={3}
+            modules={[ Pagination, Autoplay ]}
+            spaceBetween={ 50 }
+            slidesPerView={ 2 }
+            autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+            }}
+            centeredSlides={ true }
+            pagination={{
+                type: "franction", 
+                // clickable: true,
+            }}
             onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
+            onSwiper={( swiper ) => console.log('swiper 테스트 : ', swiper)}
         >
             <SwiperSlide>Slide 1</SwiperSlide> 
             <SwiperSlide>Slide 2</SwiperSlide>
